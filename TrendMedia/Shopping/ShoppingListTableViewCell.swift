06355b8ee11befa55f27c45bb76a197c82513097
jaehoon <sjh22058@gmail.com>
2022-07-19 23:53:01 +0900

    search 버튼 기능 추가 및 textField view 레이아웃 변경

--- TrendMedia/Shopping/ShoppingListTableViewCell.swift
+++ TrendMedia/Shopping/ShoppingListTableViewCell.swift
@@ -17,17 +17,16 @@ class ShoppingListTableViewCell: UITableViewCell {
     
     @IBAction func checkboxButtonTapped(_ sender: UIButton) {
         
-        sender.isSelected ? checkboxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected) : checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+        sender.isSelected ? checkboxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .highlighted) : checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
         
     }
     
     @IBAction func bookmarkButtonTapped(_ sender: UIButton) {
         
         sender.isSelected ? bookmarkButton.setImage(UIImage(systemName: "star.fill"), for: .selected) : bookmarkButton.setImage(UIImage(systemName: "star"), for: .normal)
-        
-        
-        
+    
     }
+    
     
     
 }
--- TrendMedia/Shopping/ShoppingTableViewController.swift
+++ TrendMedia/Shopping/ShoppingTableViewController.swift
@@ -12,6 +12,7 @@ class ShoppingTableViewController: UITableViewController {
     
     @IBOutlet weak var shoppingTextField: UITextField!
     @IBOutlet weak var restoreButton: UIButton!
+    @IBOutlet weak var mainUIView: UIView!
     
     
     
@@ -20,49 +21,64 @@ class ShoppingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 상단 뷰 설정
+        mainUIView.backgroundColor = .systemGray5
+        mainUIView.layer.cornerRadius = 8
         
-        tableView.rowHeight = 60
+        tableView.rowHeight = 56
         
         restoreButton.setTitle("추가", for: .normal)
         restoreButton.setTitleColor(.black, for: .normal)
-        restoreButton.backgroundColor = UIColor.lightGray
+        restoreButton.backgroundColor = UIColor.systemGray4
         restoreButton.layer.cornerRadius = 4
         
-        shoppingTextField.attributedPlaceholder = NSAttributedString(string: "무엇을 구매하실 건가요?", attributes: [NSAttributedString.Key.foregroundColor : UIColor.systemGray])
-        shoppingTextField.backgroundColor = .systemGray5
+        designTextField(item: shoppingTextField)
        
     }
-
+    // textField action
     @IBAction func shoppingListTextField(_ sender: UITextField) {
         
     }
     
+    // 추가 버튼 action(버튼을 누르면 textfield 비워주고, 키보드 올라와있으면 내려주고, 빈칸일 때 누르면 alert띄워주기)
     @IBAction func restoreButtonTapped(_ sender: UIButton) {
         
         guard let shopping = shoppingTextField.text else { return }
         
-        shoppingList.append(shopping)
+        shopping == "" ? showAlertController() : shoppingList.append(shopping)
+
         tableView.reloadData()
+        shoppingTextField.text = ""
+        view.endEditing(true)
     }
     
     
+    func designTextField(item : UITextField) {
+        item.attributedPlaceholder = NSAttributedString(string: "무엇을 구매하실 건가요?", attributes: [NSAttributedString.Key.foregroundColor : UIColor.systemGray])
+        item.backgroundColor = .systemGray5
+        item.borderStyle = .none
+    }
+    
+    
+    
+    // cell 개수 정해주기
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.count
     }
     
-    
+    // cell design 및 데이터
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingListTableViewCell", for: indexPath) as! ShoppingListTableViewCell
+        // cell의 design + data
+        cell.backgroundColor = .systemGray5
         
         cell.shoppingListLabel.text = shoppingList[indexPath.row]
         cell.shoppingListLabel.font = .boldSystemFont(ofSize: 13)
         
-//        cell.bookmarkButton.setImage(UIImage(systemName: "star"), for: .normal)
-//        cell.checkboxButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
-        
-        cell.backgroundColor = .systemGray5
-        
+        // cell안에있는 button 활성화
+        cell.bookmarkButton.tintColor = .black
+        cell.checkboxButton.tintColor = .black
         cell.bookmarkButtonTapped(cell.bookmarkButton)
         cell.checkboxButtonTapped(cell.checkboxButton)
         
@@ -81,16 +97,29 @@ class ShoppingTableViewController: UITableViewController {
             tableView.reloadData()
         }
     }
+    
+    // textfield 비어있을 때 alert action
+    func showAlertController() {
+        
+        let alert = UIAlertController(title: "경고", message: "다시 입력해주세요", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(ok)
+        
+        present(alert, animated: true, completion: nil)
+    }
    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20.0
+    }
+    
     
     /*
      고찰
-     
-     button Text 어떻게 지우고 시작할지?
-     버튼 눌리면서 구현하는 방법
-     section 추가하는 방법 custom cell
-     textfiled 레이아웃 다시 구현...
-     
+     line 80 -> button tapped 함수 구현
+     cell 마다 여백주기
+     uiview 사용하지 않고, textfield랑 button을 포함하는 custom cell 만드는 방법(Section 추가)
      */
     
     
